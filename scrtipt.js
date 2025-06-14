efa37b285b21ebc1d63d8ef2c73c701ab2768c81
Muhammadamin-player <ibrohimovmuhammadamin888@gmail.com>
2025-06-14 11:18:29 +0500

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,14 @@
+let number = prompt('Salom')
+let number1 = prompt('Ismingiz nima')
+console.log('tanishkanimdan hursandman' + number1);
+let number2 = prompt('Yoshingiz nechida')
+
+
+
+if(number2 >14 && number2<40) {
+    console.log('Mening (ham yoshim shunday');
+}
+else {
+    console.log('Siz mendan ancha katta ekasiz');
+    
+}
